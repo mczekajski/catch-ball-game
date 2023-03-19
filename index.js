b71eb7750009211cda06fbc1a0f0bd6240a71518
Marcin Czekajski <czekajskimarcin@gmail.com>
2023-03-19 18:16:37 +0100

    show assets

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import Canvas from "./Canvas.js";
 const { fromEvent } = rxjs;
 
-const canvas = new Canvas("canvas");
+const game = document.getElementsByClassName("game");
 
-const click$ = fromEvent(canvas.canvas, "click");
-const windowResize$ = fromEvent(window, "resize");
+// const canvas = new Canvas("canvas");
+
+const click$ = fromEvent(game, "click");
+// const windowResize$ = fromEvent(window, "resize");
 
 click$.subscribe((event) => {
   console.log(`Clicked at ${event.clientX} ${event.clientY}`);
 });
 
-windowResize$.subscribe(() => {
-  canvas.setCanvasDimensions();
-  canvas.paint();
-});
+// windowResize$.subscribe(() => {
+//   canvas.setCanvasDimensions();
+//   canvas.paint();
+// });
